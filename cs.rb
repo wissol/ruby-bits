42b3d848706c6c11d29deab031213c0db54f5e11
Miguel de Luis <luis.miguelde@gmail.com>
2017-04-17 10:17:21 +0100

    regex

--- cs.rb
+++ cs.rb
@@ -560,3 +560,43 @@ File.readlines("fruits.txt")
 File.readlines("fruits.txt").each do |line|
   some_arr << line.strip
 end
+
+# REGEX see more at rubular.com
+
+# Return MatchData object for last match
+"string".match(/.*/)
+/.*/ =~ "string"
+
+# Capturing data from or around a match
+/(.*)testing(.*)/ =~ "need more testing in here"
+
+# Return an array of matches
+"string".scan(/foo/)
+
+# Substitute the matches for something else
+"foostring".gsub(/foo/,"bar")
+
+# [abc]	A single character of: a, b, or c
+# [^abc]	Any single character except: a, b, or c
+# [a-z]	Any single character in the range a-z
+# [a-zA-Z]	Any single character in the range a-z or A-Z
+# ^	Start of line
+# $	End of line
+# \A	Start of string
+# \z	End of string
+# .	Any single character
+# \s	Any whitespace character
+# \S	Any non-whitespace character
+# \d	Any digit
+# \D	Any non-digit
+# \w	Any word character (letter, number, underscore)
+# \W	Any non-word character
+# \b	Any word boundary
+# (...)	Capture everything enclosed
+# (a|b)	a or b
+# a?	Zero or one of a
+# a*	Zero or more of a
+# a+	One or more of a
+# a{3}	Exactly 3 of a
+# a{3,}	3 or more of a
+# a{3,6}	Between 3 and 6 of a
